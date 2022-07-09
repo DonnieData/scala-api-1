5061fb4b2f50e15b850b330c8e3d6820f31a5856
Donnell Jackson <Donnell.Jackson@outlook.com>
2022-07-08 20:02:31 -0500

    testing importing object from package for config purposes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,11 @@
-
-object Main extends App with requests {
+import config1._
+object Main extends App {
     val r = requests.get("https://api.github.com/users/lihaoyi")
     println(r.statusCode)
-    println(r.headers("content-type"))
-    println(r.text)
+    //println(r.headers("content-type"))
+    //println(r.text)
+    val p1 = config1.profile1.name
+    println(p1)
+
 
   }
